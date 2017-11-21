c10b5dc4b526ab236126a0be0a86c76ce70134e4
Jannik Adam <adamjannik1001@gmail.com>
2017-11-21 12:48:07 +0100

    Formated Code for 80 chars

--- ueb03/src/Math.java
+++ ueb03/src/Math.java
@@ -25,7 +25,8 @@ public class Math {
 		// Prufung ob die Nutzereingabe eine naturliche Zahl ist.
 		// Prüft ob die uebergebene Zahl kleiner als 0 ist.
 		if (teilerSummenZahl <= 0) {
-			throw new IllegalArgumentException("Die Teilersummenzahl muss eine natuerliche Zahl sein!");
+			throw new IllegalArgumentException(
+					"Die Teilersummenzahl muss eine natuerliche Zahl sein!");
 		}
 
 		for (int teiler = 1; (teiler < secondTeiler); teiler++) {
@@ -33,14 +34,16 @@ public class Math {
 			if (teilerSummenZahl % teiler == 0) {
 				// Teilersummen treten in Paaren auf: x/y = z und z/y = x
 				secondTeiler = teilerSummenZahl / teiler;
-				teilerSumme += teiler + ((secondTeiler == teiler) ? 0 : secondTeiler);
+				teilerSumme += teiler
+						+ ((secondTeiler == teiler) ? 0 : secondTeiler);
 			}
 		}
 
 		// Prufung ob ein Uberlauf stattgefunden hat.
 		if (teilerSumme < teilerSummenZahl) {
 			// Wenn, dann wird eine Runtime Exception geworfen.
-			throw new RuntimeException("Falsches Ergebnis durch ueberlauf!");
+			throw new RuntimeException(
+					"Falsches Ergebnis durch ueberlauf!");
 		}
 		// Ausgabe fuer Teilersummen.
 		return teilerSumme;
@@ -51,15 +54,16 @@ public class Math {
 	 * errechent.
 	 * <p>
 	 * 
-	 * Eine ISBN 10 besteht aus 9 zahlen von [0-9] und der Prüfziffer [0-9,x], die
-	 * hier berechnet wird.<br>
+	 * Eine ISBN 10 besteht aus 9 zahlen von [0-9] und der Prüfziffer
+	 * [0-9,x], die hier berechnet wird.<br>
 	 * Uebergeben werden hier lediglich die ersten 9 stellen der ISBN
 	 * <p>
-	 * Zu beachten ist, dass 0 an jeder Stelle erlaubt ist, und daher führende 0en
-	 * gestattet sind!<br>
+	 * Zu beachten ist, dass 0 an jeder Stelle erlaubt ist, und daher
+	 * führende 0en gestattet sind!<br>
 	 * 
 	 * @param isbn
-	 *            long, das die ersten neun Stellen einer ISBN-10 beinhaltet.
+	 *            long, das die ersten neun Stellen einer ISBN-10
+	 *            beinhaltet.
 	 * @return String, der Prufziffer einer ISBN-10.
 	 */
 	public static String berechnePrufZifferISBN_10(long isbn) {
@@ -67,16 +71,22 @@ public class Math {
 		long prufSumme = 0;
 		String prufZiffer;
 
-		// Prufung ob die Eingabe einer ISBN-10 ihne Prufziffer entspricht wobei
-		// die isbn führende 0en haben darf, die bei der convertierung zu long verloren
+		// Prufung ob die Eingabe einer ISBN-10 ihne Prufziffer entspricht
+		// wobei
+		// die isbn führende 0en haben darf, die bei der convertierung zu
+		// long verloren
 		// gingen.
 		if (isbn < MIN_ISBN_9 || isbn > MAX_ISBN_9) {
-			throw new IllegalArgumentException(" Die einzugebende ISBN muss eine ganze 9-stellige Zahl sein!");
+			throw new IllegalArgumentException(
+					" Die einzugebende ISBN muss eine ganze 9-stellige Zahl sein!");
 		}
 
-		// Fortlaufendes Abschneiden der letzten Ziffer der 9-stelligen ISBN-10,
-		// Anwendung von Modulo 10 auf diese anschliesende Faktorisierung mit einem von
-		// 9 ablaufendem Index und anschliessende Addition mit der Prufsumme.
+		// Fortlaufendes Abschneiden der letzten Ziffer der 9-stelligen
+		// ISBN-10,
+		// Anwendung von Modulo 10 auf diese anschliesende Faktorisierung
+		// mit einem von
+		// 9 ablaufendem Index und anschliessende Addition mit der
+		// Prufsumme.
 		for (int index = 9; isbn > 0; index--) {
 			long ziffer = isbn % 10;
 			isbn = isbn / 10;
@@ -84,7 +94,8 @@ public class Math {
 		}
 
 		prufSumme = prufSumme % 11;
-		// Prufung ob fuer die Prufsumme der Sonderfall 10 gilt und entsprechende
+		// Prufung ob fuer die Prufsumme der Sonderfall 10 gilt und
+		// entsprechende
 		// Ausgabe der Prufziffer.
 		if (prufSumme < 10) {
 			prufZiffer = String.valueOf(prufSumme);
@@ -96,8 +107,8 @@ public class Math {
 	}
 
 	/**
-	 * Methode zum ermitteln der Nullstellen einer quadratischen funktion der Form
-	 * f(x)=X^2+px+q.
+	 * Methode zum ermitteln der Nullstellen einer quadratischen funktion
+	 * der Form f(x)=X^2+px+q.
 	 * 
 	 * @param faktorP
 	 *            double, Variable p einer quadratischen Gleichung.
@@ -105,39 +116,50 @@ public class Math {
 	 *            double, Variable q einer quadratischen Gleichung.
 	 * @return String, der Nullstellen einer quadratischen Gleichung.
 	 */
-	public static String berechneNullStellenEinerQuadratischenGleichung(double faktorP, double summandQ) {
+	public static String berechneNullStellenEinerQuadratischenGleichung(
+			double faktorP, double summandQ) {
 		double diskriminante;
 		String ergebnis;
 
 		diskriminante = (faktorP / 2.0d) * (faktorP / 2.0d) - summandQ;
 
 		//
-		// Runden der Diskriminante auf eine 10 nachkommerstellen um das Problem der
+		// Runden der Diskriminante auf eine 10 nachkommerstellen um das
+		// Problem der
 		// Ungenauigkeit der Gleitpunktarithmetic zu vermindern.
 		diskriminante = roundToPrecision(diskriminante);
 
-		// Fallunterscheidung auf Grund der ermittelten Diskriminante und entrsprechende
-		// Ermittlung von einer, zwei Nullstellen oder der Hinweis auf komplexe
+		// Fallunterscheidung auf Grund der ermittelten Diskriminante und
+		// entrsprechende
+		// Ermittlung von einer, zwei Nullstellen oder der Hinweis auf
+		// komplexe
 		// Nullstellen.
 		try {
 			if (diskriminante < 0.0) {
 				ergebnis = ("zu komplex!");
 			} else if (diskriminante == 0.0) {
-				ergebnis = roundToPrecision(pQFormel(faktorP, summandQ, false)) + ".";
+				ergebnis = roundToPrecision(
+						pQFormel(faktorP, summandQ, false)) + ".";
 			} else {
-				ergebnis = roundToPrecision(pQFormel(faktorP, summandQ, true)) + " und "
-						+ roundToPrecision(pQFormel(faktorP, summandQ, false)) + ".";
+				ergebnis =
+						roundToPrecision(pQFormel(faktorP, summandQ, true))
+								+ " und "
+								+ roundToPrecision(
+										pQFormel(faktorP, summandQ, false))
+								+ ".";
 			}
 		} catch (IllegalArgumentException e) {
-			// Noterkennung, falls nichterkannte Komplexe nullstelle durch prezision
+			// Noterkennung, falls nichterkannte Komplexe nullstelle durch
+			// prezision
 			ergebnis = ("zu komplex!");
 		}
-		return ("Die Nullstellen der quadratischen Gleichung f(x) fuer x*x +" + faktorP + "x + (" + summandQ + ") sind "
-				+ ergebnis);
+		return ("Die Nullstellen der quadratischen Gleichung f(x) fuer x*x +"
+				+ faktorP + "x + (" + summandQ + ") sind " + ergebnis);
 	}
 
 	/**
-	 * Methode zum Runden von double zahlen auf eine Festgelegte Prezision<br>
+	 * Methode zum Runden von double zahlen auf eine Festgelegte
+	 * Prezision<br>
 	 * 
 	 * @param number
 	 *            die gerundet werden soll
@@ -154,16 +176,18 @@ public class Math {
 	 * @param q
 	 *            double, Variable q einer quadratischen Gleichung.
 	 * @param zweiNullStellen
-	 *            boolean Ausdruck der angibt ob die funktion eine oder zwei
-	 *            Nullstellen hat.
+	 *            boolean Ausdruck der angibt ob die funktion eine oder
+	 *            zwei Nullstellen hat.
 	 * @return String, der Nullstellen der quadratischen Gleichung.
 	 * @throws IllegalArgumentException
 	 *             falls das Ergebniss NaN ist
 	 */
-	private static double pQFormel(double faktorP, double summandQ, boolean zweiNullStellen) {
+	private static double pQFormel(double faktorP, double summandQ,
+			boolean zweiNullStellen) {
 		double nullStelle;
 		nullStelle = -(faktorP / 2)
-				+ ((zweiNullStellen ? 1 : (-1)) * (java.lang.Math.sqrt((faktorP / 2) * (faktorP / 2) - summandQ)));
+				+ ((zweiNullStellen ? 1 : (-1)) * (java.lang.Math
+						.sqrt((faktorP / 2) * (faktorP / 2) - summandQ)));
 		if (nullStelle == Double.NaN) {
 			// Werfe Exception, wenn das Ergebnis komplex ist
 			throw new IllegalArgumentException("Ergebniss ist Komplex");
