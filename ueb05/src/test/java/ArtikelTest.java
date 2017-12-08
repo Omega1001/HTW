7056e444cd9fdf5128d2c52bd6a41fc05b9df0f9
Jannik Adam <adamjannik1001@gmail.com>
2017-12-08 08:14:20 +0100

    Adjusted Project 05
    Added BlueJ Project builder plugin
    Edited gitignore to ignore .DS_Store Files
    Splitted ArtikelTest methodes

--- ueb05/src/test/java/ArtikelTest.java
+++ ueb05/src/test/java/ArtikelTest.java
@@ -1,74 +1,104 @@
 import static org.junit.Assert.*;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.Before;
+
 import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
-/**
- * 
- */
-
-/**
- * @author jannik
- *
- */
 public class ArtikelTest {
 
 	protected Artikel underTest;
+
+	@Before
+	public void init() {
+		underTest = new Artikel(1234, "Test", 10, 10.0);
+	}
 
 	/**
 	 * Test method for
 	 * {@link Artikel#Artikel(int, java.lang.String, int, double)}.
 	 */
 	@Test
-	public void testArtikelIntStringIntDouble() {
+	public void testArtikelConstructor() {
 		underTest = new Artikel(1234, "Test", 12, 6.7);
 		assertEquals(1234, underTest.getArtikelNummer());
 		assertEquals("Test", underTest.getArtikelBezeichnung());
 		assertEquals(12, underTest.getArtikelBestand());
 		assertEquals(6.7, underTest.getPreis(), 0);
+	}
+
+	@Test
+	public void testArtikelConstructor2() {
 		underTest = new Artikel(1234, "Test", 12);
 		assertEquals(1234, underTest.getArtikelNummer());
 		assertEquals("Test", underTest.getArtikelBezeichnung());
 		assertEquals(12, underTest.getArtikelBestand());
 		assertThat(underTest.getPreis(), greaterThan(0.0));
+	}
+
+	@Test
+	public void testArtikelConstructor3() {
 		underTest = new Artikel(1234, "Test");
 		assertEquals(1234, underTest.getArtikelNummer());
 		assertEquals("Test", underTest.getArtikelBezeichnung());
 		assertThat(underTest.getArtikelBestand(), greaterThanOrEqualTo(0));
 		assertThat(underTest.getPreis(), greaterThan(0.0));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-				.isThrownBy(() -> new Artikel(123, "Test", 12, 6.7));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-				.isThrownBy(() -> new Artikel(12345, "Test", 12, 6.7));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-				.isThrownBy(() -> new Artikel(1234, null, 12, 6.7));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> new Artikel(1234, "", 12, 6.7));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> new Artikel(1234, " ", 12, 6.7));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> new Artikel(1234, null, -15, 6.7));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> new Artikel(1234, null, 12, 0.0));
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testArtikelConstructor4() {
+		new Artikel(123, "Test", 12, 6.7);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testArtikelConstructor5() {
+		new Artikel(12345, "Test", 12, 6.7);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testArtikelConstructor6() {
+		new Artikel(1234, null, 12, 6.7);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testArtikelConstructor7() {
+		new Artikel(1234, "", 12, 6.7);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testArtikelConstructor8() {
+		new Artikel(1234, " ", 12, 6.7);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testArtikelConstructor9() {
+		new Artikel(1234, null, -15, 6.7);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testArtikelConstructor10() {
+		new Artikel(1234, null, 12, 0.0);
+	}
 
 	/**
 	 * Test method for {@link Artikel#bucheZugang(int)}.
 	 */
 	@Test
 	public void testBucheZugang() {
-		underTest = new Artikel(1234, "Test", 10, 10.0);
 		underTest.bucheZugang(5);
 		assertEquals(15, underTest.getArtikelBestand());
-		underTest.bucheZugang(17);
-		assertEquals(32, underTest.getArtikelBestand());
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.bucheZugang(0));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.bucheZugang(-12));
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testBucheZugang2() {
+		underTest.bucheZugang(0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testBucheZugang3() {
+		underTest.bucheZugang(-12);
 	}
 
 	/**
@@ -76,17 +106,23 @@ public class ArtikelTest {
 	 */
 	@Test
 	public void testBucheAbgang() {
-		underTest = new Artikel(1234, "Test", 10, 10.0);
 		underTest.bucheAbgang(3);
 		assertEquals(7, underTest.getArtikelBestand());
-		underTest.bucheAbgang(5);
-		assertEquals(2, underTest.getArtikelBestand());
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.bucheAbgang(0));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.bucheAbgang(-12));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.bucheAbgang(3));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testBucheAbgang2() {
+		underTest.bucheAbgang(0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testBucheAbgang3() {
+		underTest.bucheAbgang(-12);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testBucheAbgang4() {
+		underTest.bucheAbgang(11);
 	}
 
 	/**
@@ -95,34 +131,54 @@ public class ArtikelTest {
 	 */
 	@Test
 	public void testSetArtikelBezeichnung() {
-		underTest = new Artikel(1234, "Test", 10, 10.0);
 		underTest.setArtikelBezeichnung("T2");
 		assertEquals("T2", underTest.getArtikelBezeichnung());
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.setArtikelBezeichnung(null));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.setArtikelBezeichnung("  "));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.setArtikelBezeichnung(""));
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetArtikelBezeichnung2() {
+		underTest.setArtikelBezeichnung(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetArtikelBezeichnung3() {
+		underTest.setArtikelBezeichnung("  ");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetArtikelBezeichnung4() {
+		underTest.setArtikelBezeichnung("");
+	}
 
 	/**
 	 * Test method for {@link Artikel#setPreis(double)}.
 	 */
 	@Test
 	public void testSetPreis() {
-		underTest = new Artikel(1234, "Test", 10, 10.0);
 		underTest.setPreis(20.0);
-		assertEquals(20.0, underTest.getPreis(),0);
+		assertEquals(20.0, underTest.getPreis(), 0);
+	}
+
+	@Test
+	public void testSetPreis2() {
 		underTest.setPreis(19.005);
-		assertEquals(19.01, underTest.getPreis(),0);
+		assertEquals(19.01, underTest.getPreis(), 0);
+	}
+
+	@Test
+	public void testSetPreis3() {
 		underTest.setPreis(19.004);
-		assertEquals(19.01, underTest.getPreis(),0);
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.setPreis(0));
-		assertThatExceptionOfType(IllegalArgumentException.class)
-		.isThrownBy(() -> underTest.setPreis(-17));
+		assertEquals(19.01, underTest.getPreis(), 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetPreis4() {
+		 underTest.setPreis(0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetPreis5() {
+		underTest.setPreis(-17);
 	}
 
 }
