7d32be417dc98f3907617a39de52f6aa244c3fc3
Omega1001 <adamjannik1001@gmail.com>
2018-01-01 20:47:24 +0100

    Corrected some Test Cases and Renaming Project
    Fixed invalid test case, that expected top layer domain 'de' instead of
    'com'
    Switched top level domain of all test urls to 'com' to avoid further
    confusion
    Edited eclipses .project file to list 'ueb07' as name instead of 'ueb06'

--- ueb07/src/test/java/ueb07/XMLLinkFinderTest.java
+++ ueb07/src/test/java/ueb07/XMLLinkFinderTest.java
@@ -17,45 +17,52 @@ public class XMLLinkFinderTest {
 	@Test
 	public void testRun1() {
 		List<LinkContainer> underTest = runTestFile("SimpleHTML.html");
-		assertTrue("Missing link to Google",underTest.contains(new LinkContainer("Link To Google", "http://www.google.de")));
+		assertEquals("Result List contains to many items",2, underTest.size());
+		assertTrue("Missing link to Google",underTest.contains(new LinkContainer("Link To Google", "http://www.google.com")));
 		assertTrue("Missing link to Stackoverflow",underTest.contains(new LinkContainer("Link To Stackoverflow", "http://stackoverflow.com")));
 	}
 
 	@Test
 	public void testRun2() {
 		List<LinkContainer> underTest = runTestFile("HTMLAndSubElement.html");
+		assertEquals("Result List contains to many items",1, underTest.size());
 		assertTrue("Missing link to Stackoverflow",underTest.contains(new LinkContainer("Link", "http://stackoverflow.com")));
 	}
 	
 	@Test
 	public void testRun3() {
 		List<LinkContainer> underTest = runTestFile("HTMLAndText.html");
-		assertTrue("Missing link to Google",underTest.contains(new LinkContainer("Link", "http://www.google.de")));
+		assertEquals("Result List contains to many items",2, underTest.size());
+		assertTrue("Missing link to Google",underTest.contains(new LinkContainer("Link", "http://www.google.com")));
 		assertTrue("Missing link to Stackoverflow",underTest.contains(new LinkContainer("vergrabenen Link", "http://stackoverflow.com")));
 	}
 	
 	@Test
 	public void testRun4() {
 		List<LinkContainer> underTest = runTestFile("HTMLmitMehrerenAttributen.html");
-		assertTrue("Missing link to Google",underTest.contains(new LinkContainer("Dies", "http://www.google.de")));
+		assertEquals("Result List contains to many items",2, underTest.size());
+		assertTrue("Missing link to Google",underTest.contains(new LinkContainer("Dies", "http://www.google.com")));
 		assertTrue("Missing link to Stackoverflow",underTest.contains(new LinkContainer("Das", "http://stackoverflow.com")));
 	}
 	
 	@Test
 	public void testRun5() {
 		List<LinkContainer> underTest = runTestFile("HTMLNoHREF.html");
+		assertEquals("Result List contains to many items",1, underTest.size());
 		assertTrue("Missing link to Stackoverflow",underTest.contains(new LinkContainer("Dies", "http://stackoverflow.com")));
 	}
 	
 	@Test
 	public void testRun6() {
 		List<LinkContainer> underTest = runTestFile("HTMLWithAnchorLink.html");
+		assertEquals("Result List contains to many items",1, underTest.size());
 		assertTrue("Missing link to top",underTest.contains(new LinkContainer("Das", "#top")));
 	}
 	
 	@Test
 	public void testRun7() {
 		List<LinkContainer> underTest = runTestFile("HTMLwithCrumyURLS.html");
+		assertEquals("Result List contains to many items",3, underTest.size());
 		assertTrue("Missing link to Google",underTest.contains(new LinkContainer("Voll", "http://www.google.com")));
 		assertTrue("Missing link to Stackoverflow",underTest.contains(new LinkContainer("Unvoll", "www.stackoverflow.com")));
 		assertTrue("Missing link to relative page",underTest.contains(new LinkContainer("Relativer", "/somepage")));
