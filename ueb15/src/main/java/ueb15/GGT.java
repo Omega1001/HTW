2fe03b1147d3bf3192c1582b5f9fd83a615f8316
Dominik <Dominik@Dominik>
2018-04-23 23:31:05 +0200

    Removed "break" in exchange for return

--- ueb15/src/main/java/ueb15/GGT.java
+++ ueb15/src/main/java/ueb15/GGT.java
@@ -4,21 +4,15 @@ public class GGT {
 
 	public long ggTCalc(long a, long b) {
 
-		if (a == 0 || b == 0) {
-
-			throw new IllegalArgumentException("Value must not be 0!");
-		}
-
 		if (a < b) {
 
-			long mem = 0;
-			a = mem;
+			long aMem = a;
 			a = b;
-			b = mem;
+			b = aMem;
 			
 		}
 
-		return ggTCalculate(b, (a % b));
+		return ggTCalculate(a,b);
 
 	}
 
@@ -26,7 +20,7 @@ public class GGT {
 		
 		if (b == 0) {
 
-			return a;
+			return Math.abs(a);
 
 		}
 
