9b8ff060cb98d283a38c021f9ba631f17fac2a07
Dominik <Dominik@Dominik>
2018-04-23 23:33:17 +0200

    Removed exception

--- ueb15/src/main/java/ueb15/PalindromeIterative.java
+++ ueb15/src/main/java/ueb15/PalindromeIterative.java
@@ -4,16 +4,14 @@ public class PalindromeIterative extends AbstarctPalindrome {
 
 	@Override
 	protected boolean _isPalindrom(String word) {
-		boolean is = true;
 
 		for (int i = 0; i < word.length(); i++) {
 			if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
-				is = false;
-				break;
+				return false;
 			}
 		}
 
-		return is;
+		return true;
 	}
 	
 
