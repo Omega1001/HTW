dc1e6b44593f0cf50fe8797f972164c093d8aac5
Dominik <Dominik@DESKTOP-UQA1ACU.fritz.box>
2018-04-24 02:05:05 +0200

    The method checked each combination twice.

--- ueb15/src/main/java/ueb15/PalindromeIterative.java
+++ ueb15/src/main/java/ueb15/PalindromeIterative.java
@@ -4,8 +4,14 @@ public class PalindromeIterative extends AbstarctPalindrome {
 
 	@Override
 	protected boolean _isPalindrom(String word) {
+		
+		int stringLengthHalf = word.length()/2;
+		
+		if (word.length()%2 == 1) {
+			stringLengthHalf++;
+		}
 
-		for (int i = 0; i < word.length(); i++) {
+		for (int i = 0; i < stringLengthHalf; i++) {
 			if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
 				return false;
 			}
