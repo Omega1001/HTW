46b6d5f182a54ff41074d0d50f789d3042e22967
Dominik <Dominik@Dominik>
2018-04-24 23:08:55 +0200

    slight change

--- ueb15/src/main/java/ueb15/PalindromeRecursive2.java
+++ ueb15/src/main/java/ueb15/PalindromeRecursive2.java
@@ -2,18 +2,22 @@ package ueb15;
 
 public class PalindromeRecursive2 extends AbstarctPalindrome {
 
+	private int stringLength = 0;
 	private int stringLengthHalf = 0;
+	char [] wd;
 
 	@Override
 	protected boolean _isPalindrom(String word) {
+		stringLength = word.length();
 		stringLengthHalf = word.length() / 2;
-		return recursion(word, 0);
+		wd = word.toCharArray();
+		return recursion(0);
 	}
 
-	private boolean recursion(String word, int index) {
+	private boolean recursion(int index) {
 		if (index < stringLengthHalf) {
-			return word.charAt(index) == word.charAt(word.length() - 1
-					- index) && recursion(word, ++index);
+			return wd[index] == wd[stringLength - 1
+					- index] && recursion(++index);
 		}
 		return true;
 	}
