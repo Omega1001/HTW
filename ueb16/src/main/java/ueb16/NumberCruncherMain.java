89c2749865dec5d5509ca30da827c2df9089e832
Jannik Adam <adamjannik1001@gmail.com>
2018-04-27 14:32:19 +0200

    Added main method

---
+++ ueb16/src/main/java/ueb16/NumberCruncherMain.java
@@ -0,0 +1,12 @@
+package ueb16;
+
+public class NumberCruncherMain {
+
+	public static void main(String[] args) {
+		NumberCruncher n = new NumberCruncher(15);
+		n.crunch(args);
+		System.out.println(n);
+
+	}
+
+}
