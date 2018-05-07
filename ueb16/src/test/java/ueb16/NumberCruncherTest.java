259f891c407a11076dec6945be48106c6578ea11
Dominik <Dominik@Dominik>
2018-05-08 01:15:47 +0200

    Divide not working
    Pending
    	Main

--- ueb16/src/test/java/ueb16/NumberCruncherTest.java
+++ ueb16/src/test/java/ueb16/NumberCruncherTest.java
@@ -42,25 +42,27 @@ public class NumberCruncherTest {
 	@Test
 	public void NumberCruncherDivideTest1() {
 		underTest.crunch("DIVIDE");
-		fail("Unfinished Test");
+		arrayEquals(underTest.getNumbers(), 1f, 2f, 1, 4.5f,
+				9.99999f);
 	}
 
 	@Test
 	public void NumberCruncherSubtractTest1() {
 		underTest.crunch("SUBTRACT");
-		fail("Unfinished Test");
+		arrayEquals(underTest.getNumbers(), 1f, -1f, -6.5f, -15.5f,
+				-25.49999f);
 	}
 
 	@Test
 	public void NumberCruncherAverageTest1() {
 		underTest.crunch("AVERAGE");
-		fail("Unfinished Test");
+		arrayEquals(underTest.getNumbers(), 1f, 2f, 5.5f, 9f, 27.49999f/5f);
 	}
 
 	@Test
 	public void NumberCruncherSwirlTest1() {
 		underTest.crunch("SWIRL");
-		fail("Unfinished Test");
+		arrayValuesEquals(underTest.getNumbers(), 1f, 2f, 5.5f, 9f, 9.99999f);
 	}
 
 	private void arrayEquals(float[] numbers, float... expected) {
@@ -71,6 +73,17 @@ public class NumberCruncherTest {
 			}
 		} else {
 			fail("Array Length differs");
+		}
+	}
+	
+	private void arrayValuesEquals(float[] numbers, float... expected) {
+		if ((numbers == null && expected.length == 0)
+				|| numbers.length == expected.length) {
+			for (int i = 0; i < numbers.length; i++) {
+				for(int j = 0; numbers[i] != expected[j] && j < expected.length; j++) {
+				assertFalse(j == numbers.length-1 && numbers[i] != expected[j]);
+				}
+			}
 		}
 	}
 }
