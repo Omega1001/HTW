e269b7e9b6dd8b5a3a22187758bca630831c1e51
Jannik Adam <adamjannik1001@gmail.com>
2018-06-05 13:40:18 +0200

    Added Main Method

--- ueb19/src/main/java/ueb19/Main.java
+++ ueb19/src/main/java/ueb19/Main.java
@@ -1,12 +1,38 @@
 package ueb19;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		LinkedList<Object> o = new LinkedList<>();
-		o.add(7,new Object());
+		LinkedList<String> o = new LinkedList<>();
+		System.out.println(o.isEmpty());
+		o.add("Test 123");
+		o.add("Das Funktioniert");
+		o.add("Nicht");
+		o.remove(2);
+		o.set(0, "Super Test");
+		o.add(1, "Starte Test");
+		o.add("WX");
+		o.add("WY");
+		o.remove(o.indexOf("WX"));
+		o.remove(o.get(3));
+		o.addAll(Arrays.asList("Diese","Testdaten","Sind","Einzeln"));
+		
+		System.out.println(o.isEmpty());
+		System.out.println(o.size());
+		System.out.println(o.contains("Super Test"));
+		System.out.println(o.contains("Not so super Test"));
+		for(String s : o) {
+			System.out.println(s);
+		}
+		System.out.println(Arrays.deepToString(
+				o.toArray(new String[2])
+				));
+		System.out.println(Arrays.deepToString(
+				o.toArray(new String[20])
+				));
 	}
 
 }
